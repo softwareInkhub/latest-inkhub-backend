7264a27854e14b4246fc98bdbf769b9e3600c8f8
softwareInkhub <software.inkhub@gmail.com>
2025-02-18 14:37:42 +0530

    update

--- dist/acbda/accessor/firebaseClientAccessor.js
+++ dist/acbda/accessor/firebaseClientAccessor.js
@@ -2,26 +2,24 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.admin = exports.db = void 0;
 const firebase_admin_1 = __importDefault(require("firebase-admin"));
 exports.admin = firebase_admin_1.default;
 require("dotenv/config");
+console.log("Initializing Firebase Admin...");
+// Log the environment variable to check its value
+console.log("FIREBASE_PROJECT_ID:", process.env.FIREBASE_PROJECT_ID);
 const serviceAccount = {
-    "type": process.env.FIREBASE_TYPE,
-    "project_id": process.env.FIREBASE_PROJECT_ID,
-    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-    "private_key": process.env.FIREBASE_PRIVATE_KEY,
-    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-    "client_id": process.env.FIREBASE_CLIENT_ID,
-    "auth_uri": process.env.FIREBASE_AUTH_URI,
-    "token_uri": process.env.FIREBASE_TOKEN_URI,
-    "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL,
-    "universe_domain": process.env.FIREBASE_UNIVERSE_DOMAIN
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: (_a = process.env.FIREBASE_PRIVATE_KEY) === null || _a === void 0 ? void 0 : _a.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
 };
+console.log("Firebase Admin initialized with project ID:", serviceAccount.projectId);
 firebase_admin_1.default.initializeApp({
     credential: firebase_admin_1.default.credential.cert(serviceAccount)
 });
 const db = firebase_admin_1.default.firestore();
 exports.db = db;
+console.log("Firestore initialized.");
