eb1a728a17bb7179a191593ab434d7212f766114
softwareInkhub <software.inkhub@gmail.com>
2025-02-18 13:47:13 +0530

    update

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,23 @@
 const serverless = require('serverless-http')
 import express from 'express';
-import cors from 'cors';
+// import cors from 'cors';
 import schemaRoutes from './routes/schemaRoutes';
 import dataRoutes from './routes/dataRoutes';
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 
 // API Routes
+app.get("/",(req,res)=>{res.send("hello")})
 app.use('/api', schemaRoutes);
 app.use('/api', dataRoutes);
 
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-// });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
-module.exports.handler=serverless(app)
+// module.exports.handler=serverless(app)
